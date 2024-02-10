eb343372c0369e59ab8f4add49165be513ef157c
lutrapp <lutrapp@gmail.com>
2024-02-10 20:23:48 -0300

    first commit

---
+++ trapp/src/test/java/com/vc/trapp/TrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vc.trapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
